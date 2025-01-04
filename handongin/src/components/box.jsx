3000cb05cf36c9a424d102da7ef3ad6d6ca7bdcf
wonjung0305 <wonjung1919@gmail.com>
2025-01-04 09:56:25 +0900

    김원중
    score- 총 게시물 및 미해결 현황
    box- 게시물 내용
    MyHeader- 페이지 연결한거
    Selection- 분실한 물품, 보관중인 물품 선택

--- handongin/src/components/box.jsx
+++ handongin/src/components/box.jsx
@@ -1,24 +1,33 @@
 import styled from 'styled-components';
 
-function ContentBox({ status, postnum, title, type,  location, date }){
-  const LimitedTitle = title.length > 99 ? title.slice(0, 99) + "..." : title;
-  return(
+function ContentBox({ source, postnum, title, category, location, date, solved }) {
+  const LimitedTitle = title.length > 99 ? title.slice(0, 99) + '...' : title;
+
+  // 상태 결정
+  const status = solved
+    ? '해결완료'
+    : source === 'FoundItems'
+    ? '보관'
+    : '분실';
+    console.log(source)
+
+  return (
     <BoxContainer status={status}>
       <Left>
         <Status status={status}>{status}</Status>
         <Postnum>{postnum}</Postnum>
       </Left>
-      
+
       <Title>{LimitedTitle}</Title>
-      
+
       <Right>
         <TypeLocation>
-          {type} | {location}
+          {category} | {location}
         </TypeLocation>
         <Date>{date}</Date>
       </Right>
     </BoxContainer>
-  )
+  );
 }
 
 export default ContentBox;
@@ -27,22 +36,27 @@ const BoxContainer = styled.div`
   display: flex;
   margin: 10px auto;
   justify-content: space-between;
-  align-items:center;
+  align-items: center;
 
   width: 1400px;
   height: 95px;
   padding: 15px;
-  border: 3px solid  ${(props) => props.status === '분실' ? '#FF7473' : props.status === '보관' ? '#47B8E0' : '#A0A0A0'};
+  border: 3px solid
+    ${(props) =>
+      props.status === '해결완료'
+        ? '#A0A0A0'
+        : props.status === '분실'
+        ? '#FF7473'
+        : '#47B8E0'};
   background-color: #ffffff;
   border-radius: 10px;
-
-`
+`;
 
 const Left = styled.div`
-  gap:10px;
+  gap: 10px;
   width: 80px;
-  text-align:center;
-`
+  text-align: center;
+`;
 
 const Right = styled.div`
   display: flex;
@@ -50,40 +64,41 @@ const Right = styled.div`
   align-items: flex-end;
   gap: 4px;
   padding-right: 30px;
-`
+`;
 
 const Status = styled.div`
   color: ${(props) =>
-    props.status === '분실' ? '#FF7473' : props.status === '보관' ? '#47B8E0' : '#A0A0A0'};
+    props.status === '해결완료'
+      ? '#A0A0A0'
+      : props.status === '분실'
+      ? '#FF7473'
+      : '#47B8E0'};
   font-weight: bold;
   font-size: 14px;
-  
-`
+`;
 
 const Postnum = styled.div`
   font-size: 17px;
-  color: #7E6363;
-`
+  color: #7e6363;
+`;
 
 const Title = styled.div`
   flex: 1;
-  padding-left:20px;
+  padding-left: 20px;
 
   font-size: 17px;
   font-weight: bold;
   color: #000000;
-
-  
-`
+`;
 
 const TypeLocation = styled.div`
-    font-size:17px;
-    color: #000000;
-    font-weight: bold;
-`
+  font-size: 17px;
+  color: #000000;
+  font-weight: bold;
+`;
 
 const Date = styled.div`
   font-size: 17px;
   font-weight: bold;
   color: #000000;
-`
+`;
--- handongin/src/components/myHeader.jsx
+++ handongin/src/components/myHeader.jsx
@@ -1,129 +1,156 @@
-import styled from 'styled-components'
-import logo from '../../assets/logo.png'
-import report from '../../assets/report.png'
-import profile from '../../assets/profile.png'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import logo from '../../assets/logo.png';
+import report from '../../assets/report.png';
+import profile from '../../assets/profile.png';
+import { collection, getDocs } from 'firebase/firestore';
+import db from '../../firebase/firebase'; // Firebase DB 연결
 
 import Score from './score';
 import Selection from './Selection';
 import ContentBox from './box';
 
+function MyHeader() {
+  const [tab, setTab] = useState('lost'); // 현재 선택된 탭
+  const [items, setItems] = useState([]); // 전체 데이터
 
-function Header() {
+  // Firebase 데이터 가져오기
+  const fetchData = async () => {
+    try {
+      const collectionRef = tab === 'lost' ? collection(db, 'lostItems') : collection(db, 'FoundItems');
+      const snapshot = await getDocs(collectionRef);
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        source: doc.ref.parent.id, // 컬렉션 이름 가져오기
+        ...doc.data(),
+      }));
+      setItems(data);
+    } catch (error) {
+      console.error('Firebase 데이터 가져오기 실패:', error);
+    }
+  };
+  
+  
+
+  useEffect(() => {
+    fetchData();
+  }, [tab]);
+
+  // 탭 변경 핸들러
+  const handleTabChange = (selectedTab) => {
+    setTab(selectedTab);
+  };
 
   return (
-    <>
-        <div style={{width: '100%', height: '100%'}}>
-            <HeaderContainer>
-                <Img src={logo} Width='404px' Height='119px' Cursor='pointer'></Img>
-                <MenuContainer MarginLeft='168px'>
-                    <MenuText>보관중인 분실물</MenuText>
-                </MenuContainer>
-                <MenuContainer MarginLeft='62px'>
-                    <MenuText>분실물 요청</MenuText>
-                </MenuContainer>
-                <MenuContainer MarginLeft='62px'>
-                    <MenuText>분실물 신고</MenuText>
-                </MenuContainer>
-                <NameContainer MarginLeft='260px'>
-                    <MenuText>loy** 님</MenuText>
-                </NameContainer>
-                <ReportContainer>
-                    <MenuText>신고내역 조회</MenuText>
-                    <Img src={report} Width='24px' Height='24px'></Img>
-                </ReportContainer>
-                <Img src={profile} Width='52px' Height='52px' MarginLeft='19px' MarginBottom='25px'></Img>
-            </HeaderContainer>
+    <div style={{ width: '100%', height: '100%' }}>
+      <HeaderContainer>
+        <Img src={logo} Width="404px" Height="119px" Cursor="pointer"></Img>
+        <MenuContainer MarginLeft="168px">
+          <MenuText>보관중인 분실물</MenuText>
+        </MenuContainer>
+        <MenuContainer MarginLeft="62px">
+          <MenuText>분실물 요청</MenuText>
+        </MenuContainer>
+        <MenuContainer MarginLeft="62px">
+          <MenuText>분실물 신고</MenuText>
+        </MenuContainer>
+        <NameContainer MarginLeft="260px">
+          <MenuText>loy** 님</MenuText>
+        </NameContainer>
+        <ReportContainer>
+          <MenuText>신고내역 조회</MenuText>
+          <Img src={report} Width="24px" Height="24px"></Img>
+        </ReportContainer>
+        <Img src={profile} Width="52px" Height="52px" MarginLeft="19px" MarginBottom="25px"></Img>
+      </HeaderContainer>
 
-            <Score />
-            <Selection />
-            <ContentBox
-                status= "분실"
-                postnum = "1058"
-                title="1111111111222222222233333333334444444444555555555566666666667777777777888888888899999999991111111111"
-                type="카드"
-                location="현동홀"
-                date="2024.12.30"
-            />
-            <ContentBox
-                status="보관"
-                postnum = "30"
-                title = "너무 어렵다"
-                type = "스마트폰"
-                location = "느헤미야 홀"
-                date =" 2024.12.31"
-            />
-            <ContentBox
-                status="해결완료"
-                postnum = "18"
-                title = "됐어"
-                type = "학생증"
-                location = "상상랩 2"
-                date =" 2024.12.31"
-            />
-        </div>
-    </>
-  )
+      <Score />
+      <Selection TabChange={handleTabChange} />
+      <ContentList>
+  {items.map((item) => (
+    <ContentBox
+      key={item.id}
+      source={item.source} // 소속 정보 전달
+      solved={item.solved}
+      postnum={item.postnum}
+      category={item.category}
+      title={item.title}
+      location={item.place}
+      date={item.date}
+    />
+  ))}
+</ContentList>
+
+    </div>
+  );
 }
 
-export default Header
+export default MyHeader;
 
 const HeaderContainer = styled.div`
-    width: 1920px;
-    height: 133px;
-    display: flex;
-    flex-direction: row;
-    align-items: flex-end;
-    border-bottom: 1px solid #D9D9D9;
-`
+  width: 1920px;
+  height: 133px;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  border-bottom: 1px solid #d9d9d9;
+`;
 
 const Img = styled.img`
-    width: ${(props) => props.Width};
-    height: ${(props) => props.Height};
-    margin-left: ${(props) => props.MarginLeft};
-    margin-bottom: ${(props) => props.MarginBottom};
-    cursor: ${(props) => props.Cursor};
-`
+  width: ${(props) => props.Width};
+  height: ${(props) => props.Height};
+  margin-left: ${(props) => props.MarginLeft};
+  margin-bottom: ${(props) => props.MarginBottom};
+  cursor: ${(props) => props.Cursor};
+`;
 
 const MenuContainer = styled.div`
-    margin-left: ${(props) => props.MarginLeft};
-    height: 83px;
-    width: 174px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  margin-left: ${(props) => props.MarginLeft};
+  height: 83px;
+  width: 174px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    &:hover {
-        border-bottom: 5px solid #FF7473;
-        cursor: pointer;
-    }
-`
+  &:hover {
+    border-bottom: 5px solid #ff7473;
+    cursor: pointer;
+  }
+`;
 
 const MenuText = styled.p`
-    color: #34314C;
-    text-align: center;
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-`
+  color: #34314c;
+  text-align: center;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
 const NameContainer = styled.div`
-    margin-left: ${(props) => props.MarginLeft};;
-    height: 92px;
-    width: 174px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  margin-left: ${(props) => props.MarginLeft};
+  height: 92px;
+  width: 174px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const ReportContainer = styled.div`
-    width: 157px;
-    height: 45px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    background-color: #FFC952;
-    border-radius: 10px;
-    margin-bottom: 25px;
-    cursor: pointer;
-`
+  width: 157px;
+  height: 45px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffc952;
+  border-radius: 10px;
+  margin-bottom: 25px;
+  cursor: pointer;
+`;
+
+const ContentList = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  margin: 20px auto;
+`;
