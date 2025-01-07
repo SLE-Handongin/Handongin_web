ac0a158a12fe6119c74e4e8ab8a85feb19e09815
LeeOnYu <loy7153@gmail.com>
2025-01-07 20:41:58 +0900

    add img

---
+++ handongin/src/pages/mypage.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import MyHeader from "../components/writingpage/MyHeader";
+
+
+function Mypage() {
+
+  return (
+    <>
+    <MyHeader/>
+
+    </>
+  )
+}
+
+export default Mypage;
